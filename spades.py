7cb6de1ddb5842d40674c77717dcb7f794e24043
Chris <chrisjagielski@gmail.com>
2022-12-28 12:33:18 -0600

    begin spades with deck of cards

---
+++ spades.py
@@ -0,0 +1,11 @@
+# Spades game
+from deck import deck
+
+class spades(object):
+    playerName = ""
+
+if __name__ == "__main__":
+    print("Let's play Spades!")
+
+    d = deck()
+    deck.deal(d)
