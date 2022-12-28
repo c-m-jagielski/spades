473ce7c6b331bb7b47dbc62d03c93036823139cc
Chris <chrisjagielski@gmail.com>
2022-12-28 08:53:30 -0600

    stub to deal the cards

--- deck.py
+++ deck.py
@@ -1,6 +1,7 @@
 # python code for a deck of cards
 
 import json
+import random
 
 class deck(object):
 
@@ -11,13 +12,36 @@ class deck(object):
     H = "hearts"
     suits = [S, C, D, H]
 
+    def shuffle(self):
+        """
+        Shuffle the cards so that the imported deck is now in a random order
+        """
+        print "blah"
+
     def deal(self, numPlayers=4):
         """
         Deal the cards. Default to 4 players.
         """
+        f = open('cards.json')
+        data = json.load(f)
+        print data
+
+        player1 = []
+        player2 = []
+        player3 = []
+        player4 = []
+
+        i = 0
+        while i < self.size:
+            x1 = random.randint(1, self.size)
+            print x1
+            i += 1
 
 if __name__ == "__main__":
     print("init")
     f = open('cards.json')
     data = json.load(f)
-    print data
+    #print data
+
+    d = deck()
+    d.deal()
