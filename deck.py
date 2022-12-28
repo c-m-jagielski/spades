20056681cbe7a540bf0a1fdf11da43bd4afc9429
Chris <chrisjagielski@gmail.com>
2022-12-28 12:19:51 -0600

    shuffle cards

--- deck.py
+++ deck.py
@@ -12,11 +12,24 @@ class deck(object):
     H = "hearts"
     suits = [S, C, D, H]
 
-    def shuffle(self):
+    def shuffle(self, cards=None):
         """
         Shuffle the cards so that the imported deck is now in a random order
         """
-        print "blah"
+        if cards==None: return None
+
+        i = 0
+        tmp = cards
+        new = []
+        while i < self.size:
+            x1 = random.randint(0, len(tmp)-1)
+            #print x1
+            new.append(tmp[x1])
+            tmp.pop(x1)
+            i += 1
+        #print new
+        #print tmp
+        #print len(new)
 
     def deal(self, numPlayers=4):
         """
@@ -24,18 +37,15 @@ class deck(object):
         """
         f = open('cards.json')
         data = json.load(f)
-        print data
+        #print data
 
         player1 = []
         player2 = []
         player3 = []
         player4 = []
 
-        i = 0
-        while i < self.size:
-            x1 = random.randint(1, self.size)
-            print x1
-            i += 1
+        newCards = self.shuffle(data)
+
 
 if __name__ == "__main__":
     print("init")
