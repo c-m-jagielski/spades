52865d3e7be6d799d924f7068da7f409df742f4a
Chris <chrisjagielski@gmail.com>
2022-12-29 21:06:25 -0600

    obtain bids

--- spades.py
+++ spades.py
@@ -1,5 +1,6 @@
 # Spades game
 from deck import deck
+import random
 
 class spades(object):
 
@@ -7,19 +8,54 @@ class spades(object):
     numPlayers = 4
     d = None
     playerCards = {}
+    playerBids = {"p1":None, "p2":None, "p3":None, "p4":None}
 
-    def startGame(object):
+    def startGame(self):
         """
         """
+        # Start the game by dealing
         d = deck()
         playerCards = deck.deal(d, numPlayers=4, debug=True)
 
-    def reset(object):
+        # Show the user their hand
+        self.displayHand(playerCards["p1"])
+
+        # Assume the user is going to bid first
+        p1bid = raw_input("What is your bid? ")
+        #TODO error checking for integer between 0 and 13
+        #TODO allow "null", "nello", and variations too
+
+        # Obtain bids from the CPU users
+        p2bid = self.obtainCpuBid(playerCards["p2"])
+        p3bid = self.obtainCpuBid(playerCards["p3"])
+        p4bid = self.obtainCpuBid(playerCards["p4"])
+
+        print "You bid ", p1bid
+        print "P 2 bid ", p2bid
+        print "P 3 bid ", p3bid
+        print "P 4 bid ", p4bid
+
+    def reset(self):
         """
         Reset variables in this class
         """
         d = None
         playerCards = {}
+
+    def displayHand(self, myCards):
+        """
+        Sort then print user's cards
+        """
+        # TODO
+        print "\n\n", myCards
+
+    def sortHand(self, hand):
+        # TODO
+        pass
+
+    def obtainCpuBid(self, hand):
+        # TODO
+        return random.randint(0,4)
 
 if __name__ == "__main__":
     print("Let's play Spades!")
